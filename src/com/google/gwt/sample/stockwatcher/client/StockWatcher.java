a080587b9b797232285441496ecef6f31c33d406
Ken Lau <ken_lau177@hotmail.com>
2014-05-20 22:36:37 -0700

    MORE CHANGES

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -253,7 +253,7 @@ public class StockWatcher implements EntryPoint {
     
  // Display timestamp showing last refresh.
     lastUpdatedLabel.setText("Last update : "  + DateTimeFormat.getMediumDateTimeFormat().format(new Date()) + 
-    	"Conflict Here Time: UTC" + "Ken Branch Edit1");
+    	"Conflict Here Time: UTC" + "Ken Branch Edit1" + "MORE CHANGES");
  
   
   }
